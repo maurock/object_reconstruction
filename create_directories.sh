35571e8661ec71c1513c3cecf45527fd5ad98485
maurock <maurocomi92@gmail.com>
2022-09-28 15:45:59 +0100

    adapted create_directories

--- create_directories.sh
+++ create_directories.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 # Create directories if they do not exist
-mkdir -p tactile_gym_sim2real/data_collection/sim/active_reconstruction/data
-mkdir -p tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/checkpoints
-mkdir -p tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/obj_pointcloud
-mkdir -p tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/objects
-mkdir -p tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/touch_charts
-mkdir -p tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/checkpoints/touch_model
-mkdir -p tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/checkpoints/deformation_model
+mkdir -p object_reconstruction/data
+mkdir -p object_reconstruction/data/checkpoints
+mkdir -p object_reconstruction/data/obj_pointcloud
+mkdir -p object_reconstruction/data/objects
+mkdir -p object_reconstruction/data/touch_charts
+mkdir -p object_reconstruction/data/checkpoints/touch_model
+mkdir -p object_reconstruction/data/checkpoints/deformation_model
 
 
 # Add files __init__.py
-touch tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/__init__.py
-touch tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/checkpoints/__init__.py
-touch tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/obj_pointcloud/__init__.py
-touch tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/objects/__init__.py
-touch tactile_gym_sim2real/data_collection/sim/active_reconstruction/data/touch_charts/__init__.py
+touch object_reconstruction/data/__init__.py
+touch object_reconstruction/data/checkpoints/__init__.py
+touch object_reconstruction/data/obj_pointcloud/__init__.py
+touch object_reconstruction/data/objects/__init__.py
+touch object_reconstruction/data/touch_charts/__init__.py
 
